efd3bbc0c90e09bb5bd305dae221c0c25f160296
Jake King-Lee <jake.kinglee@gmail.com>
2020-11-20 17:02:20 +0000

    disable jwt

--- server.js
+++ server.js
@@ -18,42 +18,42 @@ app.use(express.json({ extended: false }));
 app.use('/api/cheats', require('./routes/api/cheats'));
 
 
-// TODO: move jwt stuff to it's own route
-// @route   GET api/token
-// @desc    Get JWT token
-// @access  Public
-app.get('/api/token', (req, res) => {
-  const uid = req.header('uid');
-  const deviceId = req.header('deviceId');
-  const exp = req.header('exp')
-  // console.log(uid);
-  // console.log(deviceId);
-  // console.log(exp);
+// // TODO: move jwt stuff to it's own route
+// // @route   GET api/token
+// // @desc    Get JWT token
+// // @access  Public
+// app.get('/api/token', (req, res) => {
+//   const uid = req.header('uid');
+//   const deviceId = req.header('deviceId');
+//   const exp = req.header('exp')
+//   // console.log(uid);
+//   // console.log(deviceId);
+//   // console.log(exp);
 
-  // If any of these values are null, send failed response
-  if (uid == null || deviceId == null || exp == null) {
-    res.sendStatus(400); // bad request
-  }
+//   // If any of these values are null, send failed response
+//   if (uid == null || deviceId == null || exp == null) {
+//     res.sendStatus(400); // bad request
+//   }
 
-  const payload = {
-    uid: uid,
-    deviceId: deviceId,
-  }
+//   const payload = {
+//     uid: uid,
+//     deviceId: deviceId,
+//   }
 
-  jwt.sign(
-    payload,
-    config.get('jwtSecret'),
-    {expiresIn: req.header('exp') },
-  (err, token) => {
-    if (err) throw err;
-    res.json({ token });
-  });
-});
+//   jwt.sign(
+//     payload,
+//     config.get('jwtSecret'),
+//     {expiresIn: req.header('exp') },
+//   (err, token) => {
+//     if (err) throw err;
+//     res.json({ token });
+//   });
+// });
 
-// ! test protected routes. will respond 'hello' if jwt token is authorised
-app.get('/api/secret', auth, (req, res) => {
-  res.send('hello');
-});
+// // ! test protected routes. will respond 'hello' if jwt token is authorised
+// app.get('/api/secret', auth, (req, res) => {
+//   res.send('hello');
+// });
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
