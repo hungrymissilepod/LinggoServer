ed5b28c3a606a9a4c299d02cc98fc82bcdc89033
Jake King-Lee <jake.kinglee@gmail.com>
2021-01-02 00:13:38 +0000

    Language route - Changed the way we update files

--- routes/api/db/language.js
+++ routes/api/db/language.js
@@ -48,6 +48,8 @@ async (req, res) => {
     id,
     words,
     questions,
+    timeStamp,
+    updated,
   }
 
   try {
@@ -59,15 +61,15 @@ async (req, res) => {
       data = await updateLanguageData(data, userData);
       return languageDataUpdateTime(res, data, timeStamp, updated);
     }
-    return res.status(200).send(createLanguageData(data, userData, timeStamp, updated));
+    return res.status(200).send(createLanguageData(data, userData));
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
 
-async function createLanguageData(data, userData, timeStamp, updated) {
-  userData.timeStamp = timeStamp; userData.updated = updated;
+async function createLanguageData(data, userData) {
+  // userData.timeStamp = timeStamp; userData.updated = updated;
   data = new LanguageModel(userData);
   await data.save();
   return data;
@@ -82,15 +84,17 @@ async function updateLanguageData(data, userData) {
   return data;
 }
 
+// ! testing out new method of simply updating timeStamp and updated to whatever we get sent. Don't think there is need to check this
 async function languageDataUpdateTime(res, data, timeStamp, updated) {
-  if (timeStamp > data.timeStamp) {
-    if (timeStamp <= new Date().getTime()) { // ensure that the [timeStamp] time sent is not in the future (compare to server time)
-      if (updated > data.updated) {
-        data = await LanguageModel.findOneAndUpdate( { uid: data.uid }, { $set: { 'timeStamp': timeStamp, 'updated': updated } }, { new: true } );
-        // data = await LanguageModel.findOneAndUpdate( { uid: data.uid, 'timeStamp': { $lt: timeStamp }, 'updated': { $lt: updated } }, { $set: { 'timeStamp': timeStamp }, $set: { 'updated': updated } }, { new: true } );
-      }
-    }
-  }
+  data = await LanguageModel.findOneAndUpdate( { uid: data.uid }, { $set: { 'timeStamp': timeStamp, 'updated': updated } }, { new: true } );
+  // if (timeStamp > data.timeStamp) {
+  //   if (timeStamp <= new Date().getTime()) { // ensure that the [timeStamp] time sent is not in the future (compare to server time)
+  //     if (updated > data.updated) {
+  //       data = await LanguageModel.findOneAndUpdate( { uid: data.uid }, { $set: { 'timeStamp': timeStamp, 'updated': updated } }, { new: true } );
+  //       // data = await LanguageModel.findOneAndUpdate( { uid: data.uid, 'timeStamp': { $lt: timeStamp }, 'updated': { $lt: updated } }, { $set: { 'timeStamp': timeStamp }, $set: { 'updated': updated } }, { new: true } );
+  //     }
+  //   }
+  // }
   return res.json(data);
 }
 
@@ -197,7 +201,9 @@ async (req, res) => {
 
     // If document does not exist
     if (!data) {
-      data = await createLanguageData(data, { uid: uid }, timeStamp, updated);
+      const userData = { uid, timeStamp, updated };
+      data = await createLanguageData(data, userData);
+      // data = await createLanguageData(data, { uid: uid }, timeStamp, updated);
     }
     // Try to find Word in array
     // Find document with mathing user id and word with matching word_id
@@ -260,7 +266,9 @@ async (req, res) => {
 
     // If document does not exist
     if (!data) {
-      data = await createLanguageData(data, { uid: uid }, timeStamp, updated);
+      const userData = { uid, timeStamp, updated };
+      data = await createLanguageData(data, userData);
+      // data = await createLanguageData(data, { uid: uid }, timeStamp, updated);
     }
 
     // Try to find Question in array
