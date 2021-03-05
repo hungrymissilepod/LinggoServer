2073cba74128e2b81f51b1fec780fa90dfd0010e
Jake King-Lee <jake.kinglee@gmail.com>
2021-03-05 23:31:35 +0000

    Added public directory

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-console.log('here');
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
