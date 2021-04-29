f08bb97df9d17f66f0b3c87233d8e8b33abe7531
Jake King-Lee <jake.kinglee@gmail.com>
2021-04-30 00:26:35 +0100

    FirebaseCloudFunctions - Added review notification functions

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const axios = require('axios');
 // Send Grid
 const sgMail = require('@sendgrid/mail');
 
@@ -23,66 +24,66 @@ sgMail.setApiKey(API_KEY);
 // npm run serve (create firebase emulator to test routes and functions. Use Postman to hit endpoints and see if function works correctly)
 // firebase deploy --only functions (deploy these functions to firebase to be used in production app)
 // firebase functions:log (to see log output from Firebase, same as can be found in the Firebase console)
-
+// firebase emulators:start --only functions (test functions locally)
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
 ///! TEST
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 ///! TEST
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
+// exports.addMessage = functions.https.onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into Firestore using the Firebase Admin SDK.
+//   const writeResult = await admin.firestore().collection('messages').add({original: original});
+//   // Send back a message that we've successfully written the message
+//   res.json({result: `Message with ID: ${writeResult.id} added.`});
+// });
 
 ///* Test email sent when we hit the http end point
-exports.testWelcomeEmail = functions.https.onRequest(async (req, res) => {
+// exports.testWelcomeEmail = functions.https.onRequest(async (req, res) => {
 
-  /// Username and userEmail from request params
-  const username = req.query.username;
-  const email = req.query.email;
+//   /// Username and userEmail from request params
+//   const username = req.query.username;
+//   const email = req.query.email;
 
-  const msg = {
-    to: email,
-    from: { name: 'Linggo', email: 'hello@linggo.io' },
-    template_id: WELCOME_EMAIL_TEMPLATE_ID,
-    dynamic_template_data: {
-      subject: 'Welcome to Linggo!',
-      name: username,
-      user_email: email,
-      user_name: username,
-      Sender_Name: 'Linggo',
-      Sender_Address: 'London',
-      Sender_City: 'UK',
-    },
-    asm: {
-      group_id: 15276,
-    },
-    mailSettings: {
-      bypass_list_management: {
-        enable: true,
-      },
-    }
-  };
+//   const msg = {
+//     to: email,
+//     from: { name: 'Linggo', email: 'hello@linggo.io' },
+//     template_id: WELCOME_EMAIL_TEMPLATE_ID,
+//     dynamic_template_data: {
+//       subject: 'Welcome to Linggo!',
+//       name: username,
+//       user_email: email,
+//       user_name: username,
+//       Sender_Name: 'Linggo',
+//       Sender_Address: 'London',
+//       Sender_City: 'UK',
+//     },
+//     asm: {
+//       group_id: 15276,
+//     },
+//     mailSettings: {
+//       bypass_list_management: {
+//         enable: true,
+//       },
+//     }
+//   };
   
-  sgMail
-    .send(msg)
-    .then(() => { console.log('Email sent'); })
-    .catch((error) => { console.error(error); console.log(error.response.body); return res.status(400).json(error).end(); });
-  res.status(200).send().end();
-});
+//   sgMail
+//     .send(msg)
+//     .then(() => { console.log('Email sent'); })
+//     .catch((error) => { console.error(error); console.log(error.response.body); return res.status(400).json(error).end(); });
+//   res.status(200).send().end();
+// });
 
 // Send email to user after signup
 exports.welcomeEmail = functions.auth.user().onCreate(async (user) => {
@@ -209,3 +210,113 @@ group_id is the the group id you want to send the email to. For example the News
 groups_to_display are the list of groups you want to display when the user clicks the Unsubscribe Preferences button in the email. We can leave this blank because all our groups are displayed by default.
 
 */
+
+///* test function to send a FCM to ALL users
+// exports.testSendToAll = functions.https.onRequest(async(req, res) => {
+//   /// Get ALL users from database
+//   var allUsers = await axios.get('https://www.api.linggo.io/api/db/user/token/all', { headers: { 'secret': functions.config().cloud_func.secret } });
+
+//   /// Convert response to array
+//   var jsonStr = JSON.stringify(allUsers.data);
+//   var users = JSON.parse(jsonStr);
+
+//   /// For each user
+//   users.forEach(function(user) {
+//     /// For each user FCM tokens
+//     user.fcmTokens.forEach(async function(token) {
+//       await admin.messaging().sendToDevice(
+//         token,
+//         {
+//           notification: {
+//             title: 'Test Message',
+//             body: `This is a test message...`
+//           }
+//         },
+//         {
+//           contentAvailable: true,
+//           priority: 'high'
+//         }
+//       ).then((response) => {
+//         // console.log('Successfully sent message:', response);
+//       }).catch((error) => {
+//         // console.log('Error sent message:', error);
+//         /// For these reasons and maybe others we should remove this token from user's token array
+//         if (error.code == 'messaging/invalid-recipient' || error.code == 'messaging/invalid-registration-token' || error.code == 'messaging/registration-token-not-registered') {
+//           axios.post('https://www.api.linggo.io/api/db/user/token/remove-token', null, { headers: { 'secret': functions.config().cloud_func.secret, 'uid': user.uid, 'fcmToken': token } });
+//         }
+//       });
+//     });
+//   });
+//   res.status(200).send();
+// });
+
+///* test function to send FCM to specific device
+// exports.testFunc = functions.https.onRequest( async (req, res) => {
+
+//   // Get JWT Token so we can talk to server
+//   var response = await axios.get('https://www.api.linggo.io/api/auth/token', { headers: { 'secret': functions.config().cloud_func.secret, 'uid': 'a4meJdw8FlQ9dCNA9LcgDWdCqfB3', 'deviceId': '1', 'exp': '1h' } });
+//   let token = response.data.token;
+//   console.log(`token: ${token}`);
+
+//   /// Get user data
+//   var response2 = await axios.get('https://www.api.linggo.io/api/db/user/global/a4meJdw8FlQ9dCNA9LcgDWdCqfB3', { headers: { 'x-auth-token': token } });
+//   // console.log(response2);
+//   let username = response2.data.username;
+//   console.log(`username: ${username}`);
+
+//   /// Send customised notification to them
+//   await admin.messaging().sendToDevice(
+//     'fGolsZqNTZm277Q6AnLEGl:APA91bEuTaQIogxUY4Gn3pZBK-VwBCWcsKq2K-_jD0GeW_a0eKfqe0Ud5a6j9PZjurjIBlpx981ksMpxyHrtLqplPZExlZ2pM1wxPk_NBeXYCUA-WJ9JCc-nC7zvDxuuwAW4uX2gjtlG',
+//     {
+//       notification: {
+//         title: 'Test Message',
+//         body: `Please enter your credit card details...`
+//       }
+//     },
+//     {
+//       contentAvailable: true,
+//       priority: 'high'
+//     }
+//   );
+
+//   res.status(200).send().end();
+// });
+
+
+/// Review Notification scheduler. Runs every hour and sends review notifications to users in their local time.
+exports.reviewNotificationScheduler = functions.pubsub.schedule('0 * * * *').onRun(async (context) => {
+  /// Get all users that need to be notified now
+  var allUsers = await axios.get('https://www.api.linggo.io/api/db/user/token/get-users-review-notifications', { headers: { 'secret': functions.config().cloud_func.secret } });
+  /// Convert response to array
+  var jsonStr = JSON.stringify(allUsers.data);
+  var users = JSON.parse(jsonStr);
+
+  /// For each user
+  users.forEach(function(user) {
+    /// For each user FCM tokens
+    user.fcmTokens.forEach(async function(token) {
+      await admin.messaging().sendToDevice(
+        token,
+        {
+          notification: {
+            body: `你好! It's time for your Chinese lesson! Let's go!`
+          }
+        },
+        {
+          contentAvailable: true,
+          priority: 'high'
+        }
+      ).then((response) => {
+        // console.log('Successfully sent message:', response);
+        console.log('Successfully sent message to user:', user.uid);
+      }).catch((error) => {
+        // console.log('Error sent message:', error);
+        console.log('Error sent message to user:', user.uid);
+        /// For these reasons and maybe others we should remove this token from user's token array
+        if (error.code == 'messaging/invalid-recipient' || error.code == 'messaging/invalid-registration-token' || error.code == 'messaging/registration-token-not-registered') {
+          axios.post('https://www.api.linggo.io/api/db/user/token/remove-token', null, { headers: { 'secret': functions.config().cloud_func.secret, 'uid': user.uid, 'fcmToken': token } });
+        }
+      });
+    });
+  });
+});
